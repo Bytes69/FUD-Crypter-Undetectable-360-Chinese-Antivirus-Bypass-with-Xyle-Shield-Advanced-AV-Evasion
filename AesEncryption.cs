d294227f2bb709179aaac20bfeb13616403cc1e1
Deveation <106935139+SPORTY123@users.noreply.github.com>
2025-02-17 17:42:11 +0200

    Update AesEncryption.cs

--- AesEncryption.cs
+++ AesEncryption.cs
@@ -8,50 +8,18 @@ public class AesEncryption
     // Method to encrypt a string
     public static string Encrypt(string plainText, byte[] key, byte[] iv)
     {
-        using (Aes aes = Aes.Create())
-        {
-            aes.Key = key;
-            aes.IV = iv;
-
-            using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
-            using (var ms = new MemoryStream())
-            {
-                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                using (var sw = new StreamWriter(cs))
-                {
-                    sw.Write(plainText);
-                }
-                return Convert.ToBase64String(ms.ToArray());
-            }
-        }
+       
     }
 
     // Method to decrypt a string
     public static string Decrypt(string cipherText, byte[] key, byte[] iv)
     {
-        using (Aes aes = Aes.Create())
-        {
-            aes.Key = key;
-            aes.IV = iv;
-
-            using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
-            using (var ms = new MemoryStream(Convert.FromBase64String(cipherText)))
-            using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
-            using (var sr = new StreamReader(cs))
-            {
-                return sr.ReadToEnd();
-            }
-        }
+       
     }
 
     // Method to generate a random key and IV
     public static (byte[] key, byte[] iv) GenerateKeyAndIV()
     {
-        using (Aes aes = Aes.Create())
-        {
-            aes.GenerateKey();
-            aes.GenerateIV();
-            return (aes.Key, aes.IV);
-        }
+        
     }
 }
